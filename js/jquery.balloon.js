4c36f288e51a68f16454432d5f508bd3fc9cc931
urin <urinkun@gmail.com>
2014-03-13 22:27:21 +0530

    Updated to 0.5.1.

--- js/jquery.balloon.js
+++ js/jquery.balloon.js
@@ -6,7 +6,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * @author: Hayato Takenaka (http://urin.github.com)
- * @version: 0.5.0 - 2014/02/17
+ * @version: 0.5.1 - 2014/03/13
 **/
 (function($) {
   //-----------------------------------------------------------------------------
@@ -198,7 +198,7 @@
     return this.each(function() {
       $target = $(this);
       (offTimer = $target.data("offTimer")) && clearTimeout(offTimer);
-      var contents = $.isFunction(options.contents)
+      var contents = options.contents = $.isFunction(options.contents)
         ? options.contents()
         : (options.contents || $target.attr("title") || $target.attr("alt"));
       var isNew = !($balloon = $target.data("balloon"));
